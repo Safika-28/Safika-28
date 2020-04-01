be1da8adb00ae65b5f43ed50275a8eeb8378e0ea
Safika-28 <62501444+Safika-28@users.noreply.github.com>
2020-04-01 22:13:01 +0700

    Add files via upload

---
+++ Program Kalkulator Celcius ke Fahrenheit.cpp
@@ -0,0 +1,17 @@
+//Diskusi  pertemuan ke 4 ALGORITMA dan PEMROGRAMAN
+//Siti Safikatun UMB36 TI Genap
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float C,F;
+    cout<<"Program Konversi Suhu"<<endl;
+    cout<<"Nasukkan Nilai Celciusnya = ";
+    cin>>C;
+    F=C*1.8 + 32;
+        cout<<"Nilai Fahrenheit = "<<F;
+    return 0;
+}
+
